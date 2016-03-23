b1ee318ee590a0dc44d51dc40d1325a70a1c34da
luis mizutani <luis.mizutani@gmail.com>
2016-03-23 19:03:58 -0300

    map for bonfim

--- index.php
+++ index.php
@@ -67,7 +67,7 @@
 							<td><div id="button_carona"><a href="http://goo.gl/forms/87JyMW0t67" target="_blank">QUERO CARONA!</a></div></td>
 						</tr>
 						<tr>
-							<td><strong>Bela Vista</strong>
+							<td><strong>Bela Vista  </strong>
 								<a href="https://www.google.com.br/maps/dir/TECNOPUC+-+Partenon,+Porto+Alegre+-+RS/Carlos+Trein+Filho+-+Bela+Vista,+Porto+Alegre+-+RS/@-30.0481431,-51.1852474,15z/am=t/data=!3m1!4b1!4m24!4m23!1m15!1m1!1s0x951977fd79a847e5:0xa0f279b1e3f4bf0c!2m2!1d-51.1708715!2d-30.0605766!3m4!1m2!1d-51.1699103!2d-30.0607584!3s0x95199d57c24395d1:0xcf307595038b8b1e!3m4!1m2!1d-51.169518!2d-30.0577733!3s0x951977fd2fdee359:0xadfb535019ff0522!1m5!1m1!1s0x9519782f3fcd7331:0x6cfe95fc3c8daf7a!2m2!1d-51.185946!2d-30.034618!3e0?hl=en" target="_blank">Ver mapa</a>
 							</td>
 							<td>
@@ -83,7 +83,9 @@
 							<td><div id="button_carona"><a href="http://goo.gl/forms/wNlXVAzL6N" target="_blank">QUERO CARONA!</a></div></td>
 						</tr>
 						<tr>
-							<td><strong>Bom fim</strong></td>
+							<td><strong>Bom fim  </strong>
+								<a href="https://www.google.com.br/maps/dir/TECNOPUC+-+Partenon,+Porto+Alegre+-+RS/R.+Castro+Alves+-+Rio+Branco,+Porto+Alegre+-+RS,+90430-131/@-30.0466386,-51.1943547,15z/am=t/data=!3m1!4b1!4m19!4m18!1m10!1m1!1s0x951977fd79a847e5:0xa0f279b1e3f4bf0c!2m2!1d-51.1708715!2d-30.0605766!3m4!1m2!1d-51.168213!2d-30.0587225!3s0x951977e2b41b3051:0x88311f0f21437f8c!1m5!1m1!1s0x951979b4c2779f2b:0xc8efd031a2567be5!2m2!1d-51.2032468!2d-30.0312229!3e0?hl=en" target="_blank">Ver Mapa</a>
+							</td>
 							<td>
 								<ol>
 							    	<li>Origem: Tecnopuc</li>
