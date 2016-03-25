385b706d49ab0d3cca0e16a526cfbd88d27127e2
luis mizutani <luis.mizutani@gmail.com>
2016-03-25 00:28:37 -0300

    div test

--- bonfim.php
+++ bonfim.php
@@ -8,7 +8,8 @@
 		<link rel="stylesheet" type="text/css" href="route_page.css">
 	</head>
 	<body>
-	<div width="50%" display="block" float="left">
+	<div class="main">
+		adasd
 	</div>
 	<div class="form">
 		<iframe src="https://docs.google.com/a/thoughtworks.com/forms/d/1J4EZwgVo9rcoq2n433AzAbHArPuWvElqFbk0NRgzMaM/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" scrolling="no" align="right">Loading...</iframe>
