925291acb8754e594d058c74ff082935f2e10b97
luis mizutani <luis.mizutani@gmail.com>
2016-03-25 00:34:26 -0300

    new css file

--- bonfim.php
+++ bonfim.php
@@ -9,7 +9,8 @@
 	</head>
 	<body>
 	<div class="main">
-		adasd
+		<div class="motorista"></div>
+		<div class="map"></div>
 	</div>
 	<div class="form">
 		<iframe src="https://docs.google.com/a/thoughtworks.com/forms/d/1J4EZwgVo9rcoq2n433AzAbHArPuWvElqFbk0NRgzMaM/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" scrolling="no" align="right">Loading...</iframe>
