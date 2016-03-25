6c1698f440a3039302b9746a474f99df59947928
luis mizutani <luis.mizutani@gmail.com>
2016-03-24 23:31:27 -0300

    div test

--- bonfim.php
+++ bonfim.php
@@ -8,9 +8,9 @@
 		<link rel="stylesheet" type="text/css" href="route_page.css">
 	</head>
 	<body>
-	<div style="width: 50%; display: block; float:left">
+	<div width="50%" display="block" float="left">
 	</div>
-	<div style="width: 30%; display: block; float:left">
+	<div width="30%" display="block" float="left">
 		<iframe src="https://docs.google.com/a/thoughtworks.com/forms/d/1J4EZwgVo9rcoq2n433AzAbHArPuWvElqFbk0NRgzMaM/viewform?embedded=true" width="640" height="1400" frameborder="0" marginheight="0" marginwidth="0" scrolling="no">Loading...</iframe>
 	</div>
 	</body>
