ef652a5d0ccb0478ace111ff97c20e4e6668a06d
luis mizutani <luis.mizutani@gmail.com>
2016-03-25 01:27:12 -0300

    map size

--- bonfim.php
+++ bonfim.php
@@ -21,7 +21,7 @@
  			<p><strong>Horário:</strong> 19:00</p>
 		</div>
 		<div class="map">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d27629.347963284792!2d-51.20311400954654!3d-30.046367040405862!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x951977fd79a847e5%3A0xa0f279b1e3f4bf0c!2sTECNOPUC+-+Avenida+Ipiranga%2C+6681+-+212+-+Partenon%2C+Porto+Alegre+-+RS%2C+90619-900!3m2!1d-30.060576599999997!2d-51.1708715!4m5!1s0x951979b4c2779f2b%3A0xc8efd031a2567be5!2sR.+Castro+Alves+-+Independ%C3%AAncia%2C+Porto+Alegre+-+RS!3m2!1d-30.031125199999998!2d-51.2037145!5e0!3m2!1sen!2sbr!4v1458877421982" width="800" height="600" frameborder="0" style="border:0" allowfullscreen>map</iframe>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d27629.347963284792!2d-51.20311400954654!3d-30.046367040405862!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x951977fd79a847e5%3A0xa0f279b1e3f4bf0c!2sTECNOPUC+-+Avenida+Ipiranga%2C+6681+-+212+-+Partenon%2C+Porto+Alegre+-+RS%2C+90619-900!3m2!1d-30.060576599999997!2d-51.1708715!4m5!1s0x951979b4c2779f2b%3A0xc8efd031a2567be5!2sR.+Castro+Alves+-+Independ%C3%AAncia%2C+Porto+Alegre+-+RS!3m2!1d-30.031125199999998!2d-51.2037145!5e0!3m2!1sen!2sbr!4v1458877421982" width="650" height="500" frameborder="0" style="border:0" allowfullscreen align="center">map</iframe>
 		</div>
 	</div>
 	<div class="form">
