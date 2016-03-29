1877bdde0146d4c082af5085aa65b15bf335a053
luis mizutani <luis.mizutani@gmail.com>
2016-03-29 18:53:37 -0300

    fix in css

--- bomfim.php
+++ bomfim.php
@@ -21,27 +21,29 @@
 					</div>
 				</div>
 			</div>
-			<div class="main">
-				<div class="route_top">
-					<h3>Rota: Bom Fim</h3>
-				</div>	
-				<div class="motorista_pic">
-					<img src="images/mizu.jpg">
-					<p>Luis Mizutani</p>
+			<div id="content-wrapper">
+				<div class="main">
+					<div class="route_top">
+						<h3>Rota: Bom Fim</h3>
+					</div>	
+					<div class="motorista_pic">
+						<img src="images/mizu.jpg">
+						<p>Luis Mizutani</p>
+					</div>
+					<div class="motorista_info">
+						<h4>Dados do trajeto</h4>
+			 			<p><strong>Origem:</strong> Tecnopuc</p>
+			 			<p><strong>Destino:</strong> Castro Alves</p>
+			 			<p><strong>Dias:</strong> Seg a Sex</p>
+			 			<p><strong>Horário:</strong> 19:00</p>
+					</div>
+					<div class="map">
+						<iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d27629.347963284792!2d-51.20311400954654!3d-30.046367040405862!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x951977fd79a847e5%3A0xa0f279b1e3f4bf0c!2sTECNOPUC+-+Avenida+Ipiranga%2C+6681+-+212+-+Partenon%2C+Porto+Alegre+-+RS%2C+90619-900!3m2!1d-30.060576599999997!2d-51.1708715!4m5!1s0x951979b4c2779f2b%3A0xc8efd031a2567be5!2sR.+Castro+Alves+-+Independ%C3%AAncia%2C+Porto+Alegre+-+RS!3m2!1d-30.031125199999998!2d-51.2037145!5e0!3m2!1sen!2sbr!4v1458877421982" width="650" height="450" frameborder="0" style="border:0" allowfullscreen align="center">map</iframe>
+					</div>
 				</div>
-				<div class="motorista_info">
-					<h4>Dados do trajeto</h4>
-		 			<p><strong>Origem:</strong> Tecnopuc</p>
-		 			<p><strong>Destino:</strong> Castro Alves</p>
-		 			<p><strong>Dias:</strong> Seg a Sex</p>
-		 			<p><strong>Horário:</strong> 19:00</p>
+				<div class="form">
+					<iframe src="https://docs.google.com/a/thoughtworks.com/forms/d/1J4EZwgVo9rcoq2n433AzAbHArPuWvElqFbk0NRgzMaM/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" scrolling="no" align="right">form</iframe>
 				</div>
-				<div class="map">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d27629.347963284792!2d-51.20311400954654!3d-30.046367040405862!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x951977fd79a847e5%3A0xa0f279b1e3f4bf0c!2sTECNOPUC+-+Avenida+Ipiranga%2C+6681+-+212+-+Partenon%2C+Porto+Alegre+-+RS%2C+90619-900!3m2!1d-30.060576599999997!2d-51.1708715!4m5!1s0x951979b4c2779f2b%3A0xc8efd031a2567be5!2sR.+Castro+Alves+-+Independ%C3%AAncia%2C+Porto+Alegre+-+RS!3m2!1d-30.031125199999998!2d-51.2037145!5e0!3m2!1sen!2sbr!4v1458877421982" width="650" height="450" frameborder="0" style="border:0" allowfullscreen align="center">map</iframe>
-				</div>
-			</div>
-			<div class="form">
-				<iframe src="https://docs.google.com/a/thoughtworks.com/forms/d/1J4EZwgVo9rcoq2n433AzAbHArPuWvElqFbk0NRgzMaM/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" scrolling="no" align="right">form</iframe>
 			</div>
 		</div>
 	</body>
