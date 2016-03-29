de6155b23a5ba9bbae8c1b43b5a8b5c0754a2d45
luis mizutani <luis.mizutani@gmail.com>
2016-03-29 19:07:21 -0300

    fix in css

--- bomfim.php
+++ bomfim.php
@@ -21,7 +21,7 @@
 					</div>
 				</div>
 			</div>
-			<div id="content_wrapper">
+			<div id="content_wrapper" style="margin: auto; width: 90%">
 				<div class="main">
 					<div class="route_top">
 						<h3>Rota: Bom Fim</h3>
