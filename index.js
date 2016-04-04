cd759d18077995423e088426fb7f19becde954f9
Jefferson Stachelski <jeffhsta@riseup.net>
2016-04-04 20:32:22 -0300

    Replace 4 spaces to 2 spaces

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-// import express from 'express';
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/index.html`);
+  res.sendFile(`${__dirname}/index.html`);
 });
 
 app.use(express.static(`${__dirname}/public`));
 app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`);
+  console.log(`Listening on ${PORT}`);
 });
