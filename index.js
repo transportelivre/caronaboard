cf2c5ae8755070da64d609439ec1e7d7bd85b8dc
Jefferson Stachelski <jeffhsta@riseup.net>
2016-04-04 20:47:57 -0300

    Creates an index2.html for change to use bootstrap

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
+app.get('/v2', (req, res) => {
+  res.sendFile(`${__dirname}/index2.html`);
+});
+
 app.use(express.static(`${__dirname}/public`));
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
