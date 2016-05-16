25bd434aee30c112f6cd94d37881a2ad3e210fa6
Lucas <llago@thoughtworks.com>
2016-05-16 18:57:42 -0300

    add tests for App component

---
+++ src/public/js/App-spec.js
@@ -0,0 +1,25 @@
+import React        from 'react';
+import {shallow}    from 'enzyme';
+import {expect}     from 'chai';
+import App          from './App.jsx';
+
+describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App/>)
+  });
+
+  it('should render with correct id', () => {
+    expect(wrapper.find('#app-main')).to.have.length(1);
+  });
+
+  it('should render Header component', () => {
+    expect(wrapper.find('Header')).to.have.length(1);
+  });
+
+  it('should render RoutesBox component', () => {
+    expect(wrapper.find('RoutesBox')).to.have.length(1);
+  });
+
+});
