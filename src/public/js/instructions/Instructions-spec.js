6991b7481e90a361b7bb2c279d08b6804875dae1
Lucas <llago@thoughtworks.com>
2016-05-16 19:59:07 -0300

    add tests for ol element in instructions component

--- src/public/js/instructions/Instructions-spec.js
+++ src/public/js/instructions/Instructions-spec.js
@@ -13,4 +13,12 @@ describe('Instructions', () => {
   it('should render with correct id', () => {
     expect(wrapper.props().id).to.eql('how_to');
   });
+
+  it('should render instructions text correctly', () => {
+    const liElements = wrapper.find('ol li');
+    expect(liElements.at(0).text()).to.eql('Encontre uma rota que passe perto do seu destino');
+    expect(liElements.at(1).text()).to.eql('Clique no botão "Quero Carona"');
+    expect(liElements.at(2).text()).to.eql('Preencha o pedido de carona e envie o pedido');
+    expect(liElements.at(3).text()).to.eql('Pronto! Seu colega oferecendo carona para a rota selecionada irá receber sua mensagem');
+  });
 })
