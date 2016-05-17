ad8f3953234f9025d0251d65071b6893158c58da
Lucas <llago@thoughtworks.com>
2016-05-17 11:49:45 -0300

    add url for old index.html

--- src/express/server.js
+++ src/express/server.js
@@ -5,11 +5,21 @@ const path = require('path');
 const startApiServer = function(PORT) {
   const app = express();
 
-  app.use('/', express.static(path.join(`${__dirname}/../../build`)));
+  // app.use('/', express.static(path.join(`${__dirname}/../../build`)));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(`${__dirname}/../../index.html`));
+  });
+
+  app.get('/v2', (req, res) => {
+    res.sendFile(path.resolve(`${__dirname}/../../build/index.html`));
+  });
 
   app.listen(PORT, () => {
     logger.logSuccess('Express server started:', `http://localhost:${PORT}/`);
   });
+
+  app.use(express.static(path.resolve(`${__dirname}/../../build`)));
+
 };
 
 module.exports = startApiServer;
