46d75086c9e1a42fde83766e898285a4f3b45930
Lucas Lago <lucasbenderlago@gmail.com>
2016-07-28 16:35:18 -0300

    create module responsible for consuming OKTA API

---
+++ src/fetchers/OktaFetcher.js
@@ -0,0 +1,18 @@
+import OKTA_CONFIG from '../express/okta-config';
+
+export default class OktaFetcher {
+
+  constructor(requestPromise) {
+    this.requestPromise = requestPromise;
+  }
+
+  getPublicKeys() {
+    const requestOptions = {
+      method: 'GET',
+      uri: `${OKTA_CONFIG.OKTA_BASE_URL}/oauth2/v1/keys`,
+      json: true
+    };
+    return this.requestPromise(requestOptions);
+  }
+
+}
---
+++ test/fetchers/OktaFetcher-spec.js
@@ -0,0 +1,25 @@
+import OktaFetcher from '../../src/fetchers/OktaFetcher';
+import OKTA_CONFIG from '../../src/express/okta-config';
+import sinon from 'sinon';
+import {expect} from 'chai';
+
+describe('OktaFetcher', () => {
+  let requestPromiseSpy;
+  let oktaFetcher;
+
+  beforeEach(() => {
+    requestPromiseSpy = sinon.spy();
+    oktaFetcher = new OktaFetcher(requestPromiseSpy);
+  });
+
+  it('should get Okta Configuration', () => {
+    const expectedOptions = {
+       method: 'GET',
+       uri: `${OKTA_CONFIG.OKTA_BASE_URL}/oauth2/v1/keys`,
+       json: true
+     };
+    oktaFetcher.getPublicKeys();
+    expect(requestPromiseSpy.calledWith(expectedOptions)).to.be.true;
+  });
+
+});
